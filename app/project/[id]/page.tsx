54f496b37db39fbf286eddf7e594ce4aab5a05d1
unknown <cuongnguyen291201@gmail.com>
2025-06-07 13:58:20 +0700

    Update

--- app/project/[id]/page.tsx
+++ app/project/[id]/page.tsx
@@ -270,7 +270,6 @@ export default function ProjectDetailPage() {
             {/* Conversations List */}
 
             <div className="space-y-3">
-              <h3 className="text-3xl font-bold tracking-tight">Check list:</h3>
               {testSuites.map((testSuite) => (
                   <Card
                     key={testSuite.id}
